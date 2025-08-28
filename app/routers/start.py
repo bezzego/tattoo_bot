98fdc0f4dbedbe594805fcb91ea59995f18f4115
Egor Bezborodov <egor.bezborodoff@gmail.com>
2025-08-28 12:14:51 +0300

    Добавил фото к приветсвтию

--- app/routers/start.py
+++ app/routers/start.py
@@ -12,5 +12,9 @@ router = Router()
 async def command_start(message: Message, state: FSMContext):
     # If user is in any state (for example returning to start), clear it
     await state.clear()
-    # Send welcome text with the main menu keyboard
-    await message.answer(texts.WELCOME_TEXT, reply_markup=main_keyboard.main_menu())
+    # Send welcome photo with caption and the main menu keyboard
+    await message.answer_photo(
+        photo="AgACAgIAAxkBAAID0GiwHS5byqQDybHBMglOuEc2P8UDAAI19TEbq6GISeheHL_fEOMfAQADAgADeQADNgQ",
+        caption=texts.WELCOME_TEXT,
+        reply_markup=main_keyboard.main_menu(),
+    )
